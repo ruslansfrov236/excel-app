cdd294eb7ead1877cf48e3092411b0f7ca51ca43
ruslansfrov236 <ruslansfrov236@gmail.com>
2024-08-06 20:31:48 +0400

    integrasion excel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation, Inject, ViewChild } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { DropDownButtonModule } from '@syncfusion/ej2-angular-splitbuttons';
+
+import {  SpreadsheetAllModule, SpreadsheetModule } from '@syncfusion/ej2-angular-spreadsheet';
+
+
+
+import { SpreadsheetComponent, BeforeSaveEventArgs, BeforeOpenEventArgs } from '@syncfusion/ej2-angular-spreadsheet';
+import { getDefaultData } from './entities/data';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  encapsulation: ViewEncapsulation.None,
+
+    imports: [  RouterModule,    DropDownButtonModule,
+      SpreadsheetAllModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'excel-app';
+
+  @ViewChild('default')
+  spreadsheetObj!: SpreadsheetComponent;
+public data:Object[]=getDefaultData;
+public openUrl = 'https://services.syncfusion.com/angular/production/api/spreadsheet/open';
+public saveUrl = 'https://services.syncfusion.com/angular/production/api/spreadsheet/save';
+created() {
+    this.spreadsheetObj.cellFormat({ fontWeight: 'bold', textAlign: 'center', verticalAlign: 'middle' }, 'A1:F1');
+    this.spreadsheetObj.cellFormat({ fontWeight: 'bold' }, 'E31:F31');
+    this.spreadsheetObj.cellFormat({ textAlign: 'center' }, 'E31');
+    this.spreadsheetObj.numberFormat('$#,##0.00', 'F2:F31');
+}
 }
---
+++ src/app/entities/data.ts
@@ -0,0 +1,37 @@
+export let getDefaultData: Object[] = [
+  {
+    "createdDate": "2016-02-16",
+    "income": 34,
+    "incomeTitle": "Data_1",
+    "expense": 45,
+    "expenseTitle": "Data_@_1"
+  },
+  {
+    "createdDate": "2021-05-16",
+    "income": 34,
+    "incomeTitle": "Data_2",
+    "expense": 45,
+    "expenseTitle": "Data_@_2"
+  },
+  {
+    "createdDate": "2024-12-16",
+    "income": 34,
+    "incomeTitle": "Data_3",
+    "expense": 45,
+    "expenseTitle": "Data_@_3"
+  },
+  {
+    "createdDate": "2023-02-16",
+    "income": 34,
+    "incomeTitle": "Data_4",
+    "expense": 45,
+    "expenseTitle": "Data_@_4"
+  },
+  {
+    "createdDate": "2023-02-16",
+    "income": 34,
+    "incomeTitle": "Data_5",
+    "expense": 45,
+    "expenseTitle": "Data_@_5"
+  }
+];
